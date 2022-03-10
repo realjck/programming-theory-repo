d13824316cc0516e69227d199b9121bf34498276
realjck <jckleinbourg@gmail.com>
2022-03-11 00:52:21 +0100

    Add assets and build UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject splashscreen;
+    [SerializeField] private GameObject menuButtons;
+    private bool isMenuOpen;
+
+    // manage clicks
+    public void ClickButtonMenu(){
+        splashscreen.SetActive(false);
+        ShowMenu(!isMenuOpen);
+    }
+    private void ShowMenu(bool visibility){
+        menuButtons.SetActive(visibility);
+        isMenuOpen = visibility;
+    }
+
+    public void ClickAddButton(int animalIndex){
+        //TODO
+        Debug.Log("spawn animal "+animalIndex);
+        ShowMenu(false);
+    }
+}
